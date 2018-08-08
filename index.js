0ca7b26e5227f46d5486d4208ab6a83d29bb9b21
tesha <benedict.tesha@jamaatech.com>
2018-08-08 17:08:35 +0300

    Minor code improvements

--- index.js
+++ index.js
@@ -1,44 +1,39 @@
-const
-    express = require('express'),
-    fs = require('fs-extra'),
-    busboy = require('connect-busboy'),
-    path = require('path'),
-    cors = require('cors')
-    config = require('./config'),
-    shortid = require('shortid'),
-    moment = require('moment'),
-    fileStore = require('./store/file'),
-    mongoose = require('mongoose')
-    ;
+const express = require("express");
+const fs = require("fs-extra");
+const busboy = require("connect-busboy");
+const path = require("path");
+const cors = require("cors");
+const config = require("./config");
+const shortid = require("shortid");
+const moment = require("moment");
 
 const app = express();
 app.use(cors());
 app.use(busboy());
-app.use(express.static(path.join(`${__dirname}/public`)));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.post('/upload', (request, response) => {
-    request.pipe(request.busboy);
-    request.busboy.on('file', (fieldName, file, fileName) => {
-        let ext = path.extname(fileName);
-        if(ext == null) { ext = ''; }
-        //Generate a random file name
-        fileName = `${shortid.generate()}${ext}`;
-        const filePath = `${__dirname}/public/uploaded/${fileName}`;
-        const stream = fs.createWriteStream(filePath);
-        file.pipe(stream);
-        stream.on('close', () => {
-            const url = `${request.protocol}://${request.get('host')}/uploaded/${fileName}`;
-            response.json({
-                name: fileName,
-                size: fs.statSync(filePath).size,
-                date: moment().format(),
-                format: ext.replace('.', '').toUpperCase(),
-                url
-            });
-        })
+app.post("/upload", (request, response) => {
+  request.pipe(request.busboy);
+  request.busboy.on("file", (fieldName, file, fileName) => {
+    const ext = path.extname(fileName) || "";
+    //Generate a random file name
+    fileName = `${shortid.generate()}.${ext}`;
+    const filePath = `${__dirname}/public/uploaded/${fileName}`;
+    const stream = fs.createWriteStream(filePath);
+    file.pipe(stream);
+    stream.on("close", () => {
+      const url = `${request.protocol}://${request.host}/uploaded/${fileName}`;
+      response.json({
+        name: fileName,
+        size: fs.statSync(filePath).size,
+        date: moment().format(),
+        format: ext.replace(".", "").toUpperCase(),
+        url
+      });
     });
+  });
 });
 
 const server = app.listen(config.port, () => {
-    console.log('File server listening on port: ' + server.address().port);
+  console.log("File server listening on port: " + server.address().port);
 });
