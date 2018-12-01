1f8522ea6c7916c3d3c0e2a1a250a09a2955f2ca
tesha <benedict.tesha@jamaatech.com>
2018-12-01 21:41:25 +0300

    Add morgan logger

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const cors = require("cors");
 const routes = require("./routes");
+const morgan = require("morgan");
 
 const app = express();
+app.use(morgan("combined"));
 app.use(cors());
 app.use(routes);
 
